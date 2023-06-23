8f0c6a4624324aaf7c4a4690d66c463d1d445499
Oluniyisrael <israeloluniyi2004@gmail.com>
2023-06-23 17:47:25 +0100

    As ideas are comming i am coding XD

--- pages/academics/academics.js
+++ pages/academics/academics.js
@@ -1,25 +1,40 @@
-// nya nya nya nya nyaaa nyaaa
 var database = '../../markDown/database.json';
 
-function isCorrect() {
-    var username = document.getElementById('username').value
-    var password = document.getElementById('password').value
-    
-  fetch(database)
-    .then(response => response.json())
-    .then(data => {
-      for (let i = 0; i < data.length; i++) {
-       if (username === data[i].username && password === data[i].password) {
-        console.log(true)
-       }
-       console.log(false)
-      }
-    }
-    )
-
-    .catch(error => {
-      // Handle any errors that occur during the fetch
-      console.log('Error:', error);
-    });
+localStorage.removeItem('LoggedinUser')
+function incorrect(){
+    document.getElementById('Incorrect').style.display ='block'
 }
-document.getElementById('submitButton').addEventListener('click',isCorrect)
+function isCorrect() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    
+    fetch(database)
+        .then(response => response.json())
+        .then(data => {
+            if (username === '' || password === '') {
+                alert('Fill user name and password please')
+            }
+            else{
+                
+                var found = false; 
+                for (let i = 0; i < data.length; i++) {
+                    if (data[i].username === username && data[i].password === password) {
+                        found = true; 
+                        localStorage.setItem('LoggedinUser',i)
+                        break; 
+                    }
+                }
+                console.log(found); 
+                if (found === false) {
+                    incorrect()
+                }
+            }
+            })
+            .catch(error => {
+            console.log('Error:', error);
+        });
+       
+}
+
+
+document.getElementById('submitButton').addEventListener('click', isCorrect);
